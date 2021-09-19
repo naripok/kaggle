c86a024370306b1d68797adb4e0a2133f7abbd1c
naripok <fernando.canteruccio@gmail.com>
2021-09-19 09:18:43 -0300

    chore: Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> {} }:
+(pkgs.buildFHSUserEnv {
+  name = "pipzone";
+  targetPkgs = pkgs: (with pkgs; [
+    python39
+    python39Packages.pip
+    pipenv
+  ]);
+  runScript = ''
+  zsh
+  '';
+}).env
